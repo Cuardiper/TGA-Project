4c7bd48faef8e615089e97c576c61352c9a55282
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-04-24 16:04:35 +0200

    multiplicar audio

--- test_copia.cpp
+++ test_copia.cpp
@@ -82,28 +82,55 @@ int main (void)
     printf("%d\n",buffer2[166830]);
     
     
-    ///////////////////
-    //Crear un nuevo .wav con lo datos leidos
-    SNDFILE *Newsf;
-    SF_INFO Newinfo;
     
-    //comparar size de buffer para llamar correctamente
-    int *Newbuffer = sumarAudios(buffer2, info2, buffer1);
     
-    //escribir audio
     int channels = info1.channels;
     int frames = info1.frames;
     int sampleRate = info1.samplerate;
     int num_items =  frames * channels;
     
-    Newinfo.samplerate = sampleRate;
+    for (int i = 0; i < num_items; i += channels)
+    {
+        for (int j = 0; j < channels; ++j){
+            buffer1[i+j] *= 2;
+        }
+    }
+    
+    SNDFILE *Newsf;
+    SF_INFO Newinfo;
+    
+    Newinfo.samplerate = sampleRate / 2;
     Newinfo.channels = channels;
     Newinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-
-    Newsf = sf_open("new.wav", SFM_WRITE, &Newinfo);
-    int num_write = sf_write_int(Newsf, Newbuffer, num_items);
+    
+    Newsf = sf_open("new1.wav", SFM_WRITE, &Newinfo);
+    int num_write = sf_write_int(Newsf, buffer1, num_items);
     sf_close(Newsf);
     printf("Writed %d items\n",num_write);
     
+    /*
+     ///////////////////
+     //Crear un nuevo .wav con lo datos leidos
+     SNDFILE *Newsf;
+     SF_INFO Newinfo;
+     
+     //comparar size de buffer para llamar correctamente
+     int *Newbuffer = sumarAudios(buffer2, info2, buffer1);
+     
+     //escribir audio
+     int channels = info1.channels;
+     int frames = info1.frames;
+     int sampleRate = info1.samplerate;
+     int num_items =  frames * channels;
+     
+     Newinfo.samplerate = sampleRate;
+     Newinfo.channels = channels;
+     Newinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+     
+     Newsf = sf_open("new.wav", SFM_WRITE, &Newinfo);
+     int num_write = sf_write_int(Newsf, Newbuffer, num_items);
+     sf_close(Newsf);
+     printf("Writed %d items\n",num_write);
+     */
     return 0 ;
 }
