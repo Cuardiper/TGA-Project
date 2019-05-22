89ad99d71e9ee358408b3c043504439ef3821842
alexiniesta7 <iniestalex.garcia@hotmail.com>
2019-05-22 15:54:08 +0200

    New filters!

--- videoEditor.c
+++ videoEditor.c
@@ -6,6 +6,7 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <time.h>
 
 
 struct Frame {
@@ -30,6 +31,8 @@ struct Frame read_frame(char* filename) {
 
 void process_frame_bn(char* filename) {
 	struct Frame frame = read_frame(filename);
+	clock_t start,end;
+	start = clock();
 	for (int i = 0; i < frame.width*frame.height*3; i+=3)
 	{
 		int R = frame.data[i];
@@ -40,16 +43,65 @@ void process_frame_bn(char* filename) {
 		frame.data[i+1] = gray;
 		frame.data[i+2] = gray;
 	}
+	end = clock();
+	//printf("%f\n",((double) (end-start)/CLOCKS_PER_SEC));
+	//stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
+}
+
+void process_frame_binarize(char* filename) {
+	struct Frame frame = read_frame(filename);
+	for (int i = 0; i < frame.width*frame.height*3; i+=3)
+	{
+		int R = frame.data[i];
+		int G = frame.data[i+1];
+		int B = frame.data[i+2];
+		int gray = (R*0.299 + G*0.587 + B*0.114);
+		gray = gray > 127 ? 255 : 0;
+		frame.data[i]=gray;
+		frame.data[i+1] = gray;
+		frame.data[i+2] = gray;
+	}
 	stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
 }
 
-void bnFilter() {
+int max(int n1) {
+	return n1>255 ? 255 : n1;
+}
+
+
+void process_frame_sepia(char* filename) {
+	struct Frame frame = read_frame(filename);
+	for (int i = 0; i < frame.width*frame.height*3; i+=3)
+	{
+		int R = frame.data[i];
+		int G = frame.data[i+1];
+		int B = frame.data[i+2];
+		int R1 = max(R*0.383 + G*0.769 + B*0.189);
+		int G1 = max(R*0.349 + G*0.686 + B*0.168);
+		int B1 = max(R*0.272 + G*0.534 + B*0.131);
+		frame.data[i]=R1;
+		frame.data[i+1] = G1;
+		frame.data[i+2] = B1;
+	}
+	stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
+}
+
+void applyFilter(int filter) {
 	printf("Reading....\n");
 	char filename[300] = "pics/thumb1.jpg";
+	clock_t start, end;
+	start = clock();
 	for (int i = 1; i < 2586; ++i) {
 		sprintf(filename, "pics/thumb%d.jpg",i);
-		process_frame_bn(filename);
+		if (filter == 1)
+			process_frame_bn(filename);
+		else if (filter == 2)
+			process_frame_binarize(filename);
+		else if (filter == 3)
+			process_frame_sepia(filename);
 	}
+	end = clock();
+	printf("Tiempo total: %f\n",((double) (end-start)/CLOCKS_PER_SEC));
 }
 
 
@@ -70,19 +122,13 @@ int main(int argc, char* argv[])
 	int filter;
 	printf("Que filtro quieres aplicar al video?\n1 - Grayscale\n2 - Sepia Filter\n3 - Binarize Filter\n");
 	scanf("%d",&filter);
-	if (filter == 1 || filter == 3) {
-		//BN filter
-		bnFilter();
-		if (filter == 3) {
-			//Binarizar
-		}
-	}
-	else if (filter == 2) {
-	}
+	applyFilter(filter);
 	auxCommand = "ffmpeg -framerate 25 -i pics/thumb%d.jpg";
-	sprintf(comando, "%s -pattern_type glob -c:v libx264 -pix_fmt yuv420p %s_out.mp4",auxCommand, filename);
+	sprintf(comando, "%s -pattern_type glob -c:v libx264 -pix_fmt yuv420p %s_out_provisional.mp4",auxCommand, filename);
 	system(comando);
-	sprintf(comando,"ffmpeg -i video.mp4 -i audio.aac -c:v copy -c:a aac -strict experimental %s_out.mp4",filename);
+	sprintf(comando,"ffmpeg -i %s_out_provisional.mp4 -i audio.aac -c:v copy -c:a aac -strict experimental %s_out.mp4",filename,filename);
+	system(comando);
+	sprintf(comando,"rm %s_out_provisional.mp4",filename);
 	system(comando);
 	system("rm -rf pics audio.aac");
     return 0;
