9ea4dea286a037c35e02d3e6558642097413ac57
alexiniesta7 <iniestalex.garcia@hotmail.com>
2019-05-22 16:30:59 +0200

    CHanges

--- videoEditor.c
+++ videoEditor.c
@@ -7,6 +7,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #include <time.h>
+#include <dirent.h>
 
 
 struct Frame {
@@ -16,50 +17,52 @@ struct Frame {
 	int bpp;
 };
 
-struct Frame read_frame(char* filename) {
-    int width, height, bpp;
-	struct Frame result;
-    uint8_t* rgb_image = stbi_load(filename, &width, &height, &bpp, 3);
-	result.data = rgb_image;
-	result.width = width;
-	result.height = height;
-	result.bpp = bpp;
-    //stbi_image_free(rgb_image);
-    return result;
+
+
+void read_frames(struct Frame* frame, int size) {
+	for (int i = 0; i < size; ++i) {
+		char filename[300];
+		sprintf(filename, "pics/thumb%d.jpg",i);
+		int width, height, bpp;
+		uint8_t* rgb_image = stbi_load(filename, &width, &height, &bpp, 3);
+		frame[i]->data = rgb_image;
+		frame[i]->width = width;
+		frame[i]->height = height;
+		frame[i]->bpp = bpp;
+		//stbi_image_free(rgb_image);
+	}
 }
 
 
-void process_frame_bn(char* filename) {
-	struct Frame frame = read_frame(filename);
+void process_frame_bn(struct Frame* frame, char* filename) {
 	clock_t start,end;
 	start = clock();
-	for (int i = 0; i < frame.width*frame.height*3; i+=3)
+	for (int i = 0; i < (*frame).width*(*frame).height*3; i+=3)
 	{
-		int R = frame.data[i];
-		int G = frame.data[i+1];
-		int B = frame.data[i+2];
+		int R = (*frame).data[i];
+		int G = (*frame).data[i+1];
+		int B = (*frame).data[i+2];
 		int gray = (R*0.299 + G*0.587 + B*0.114);
-		frame.data[i]=gray;
-		frame.data[i+1] = gray;
-		frame.data[i+2] = gray;
+		(*frame).data[i]=gray;
+		(*frame).data[i+1] = gray;
+		(*frame).data[i+2] = gray;
 	}
 	end = clock();
 	//printf("%f\n",((double) (end-start)/CLOCKS_PER_SEC));
 	//stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
 }
 
-void process_frame_binarize(char* filename) {
-	struct Frame frame = read_frame(filename);
-	for (int i = 0; i < frame.width*frame.height*3; i+=3)
+void process_frame_binarize(struct Frame* frame, char* filename) {
+	for (int i = 0; i < (*frame).width*(*frame).height*3; i+=3)
 	{
-		int R = frame.data[i];
-		int G = frame.data[i+1];
-		int B = frame.data[i+2];
+		int R = (*frame).data[i];
+		int G = (*frame).data[i+1];
+		int B = (*frame).data[i+2];
 		int gray = (R*0.299 + G*0.587 + B*0.114);
 		gray = gray > 127 ? 255 : 0;
-		frame.data[i]=gray;
-		frame.data[i+1] = gray;
-		frame.data[i+2] = gray;
+		(*frame).data[i]=gray;
+		(*frame).data[i+1] = gray;
+		(*frame).data[i+2] = gray;
 	}
 	stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
 }
@@ -69,29 +72,28 @@ int max(int n1) {
 }
 
 
-void process_frame_sepia(char* filename) {
-	struct Frame frame = read_frame(filename);
-	for (int i = 0; i < frame.width*frame.height*3; i+=3)
+void process_frame_sepia(struct Frame* frame, char* filename) {
+	for (int i = 0; i < (*frame).width*(*frame).height*3; i+=3)
 	{
-		int R = frame.data[i];
-		int G = frame.data[i+1];
-		int B = frame.data[i+2];
+		int R = (*frame).data[i];
+		int G = (*frame).data[i+1];
+		int B = (*frame).data[i+2];
 		int R1 = max(R*0.383 + G*0.769 + B*0.189);
 		int G1 = max(R*0.349 + G*0.686 + B*0.168);
 		int B1 = max(R*0.272 + G*0.534 + B*0.131);
-		frame.data[i]=R1;
-		frame.data[i+1] = G1;
-		frame.data[i+2] = B1;
+		(*frame).data[i]=R1;
+		(*frame).data[i+1] = G1;
+		(*frame).data[i+2] = B1;
 	}
 	stbi_write_jpg(filename, frame.width, frame.height, 3, frame.data, frame.width*3);
 }
 
-void applyFilter(int filter) {
+void applyFilter(int filter, int size, struct* Frame frames) {
 	printf("Reading....\n");
-	char filename[300] = "pics/thumb1.jpg";
+	char filename[300];
 	clock_t start, end;
 	start = clock();
-	for (int i = 1; i < 2586; ++i) {
+	for (int i = 1; i < size; ++i) {
 		sprintf(filename, "pics/thumb%d.jpg",i);
 		if (filter == 1)
 			process_frame_bn(filename);
@@ -112,17 +114,30 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	char* filename = argv[1];
-	system("mkdir pics");
+	//system("mkdir pics");
 	char* auxCommand = "pics/thumb%d.jpg -hide_banner";
 	char comando[300];
 	sprintf(comando, "ffmpeg -i %s.mp4 %s",filename,auxCommand);
-	system(comando);
+	//system(comando);
 	sprintf(comando,"ffmpeg -i %s.mp4 -vn -acodec copy audio.aac",filename);
 	system(comando);
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("pics/");
+	int frames = 0;
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			frames++;
+		}
+		closedir(d);
+	}
+	printf("Total frames: %d",frames);
+	struct Frame fotogramas[frames-2];
+	read_frames(&fotogramas[0],frames-1);
 	int filter;
 	printf("Que filtro quieres aplicar al video?\n1 - Grayscale\n2 - Sepia Filter\n3 - Binarize Filter\n");
 	scanf("%d",&filter);
-	applyFilter(filter);
+	applyFilter(filter, frames-1, &fotogramas[0]);
 	auxCommand = "ffmpeg -framerate 25 -i pics/thumb%d.jpg";
 	sprintf(comando, "%s -pattern_type glob -c:v libx264 -pix_fmt yuv420p %s_out_provisional.mp4",auxCommand, filename);
 	system(comando);
@@ -130,6 +145,6 @@ int main(int argc, char* argv[])
 	system(comando);
 	sprintf(comando,"rm %s_out_provisional.mp4",filename);
 	system(comando);
-	system("rm -rf pics audio.aac");
+	system("rm audio.aac");
     return 0;
 }
