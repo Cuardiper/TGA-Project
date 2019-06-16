225b33cd876de0c1e1254661772b163bc12aeeb5
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-06-16 15:21:00 +0200

    cambio estrcutura

--- convolution3x3.c
+++ convolution3x3.c
@@ -12,8 +12,8 @@
 int kRows = 3;
 int kCols = 3;
 // double kernel[3][3] = {{(double)1/9,(double)1/9,(double)1/9}, {(double)1/9,(double)1/9,(double)1/9}, {(double)1/9,(double)1/9,(double)1/9}}; //Blur normal
-// double kernel[3][3] = {{-1,-1,-1}, {-1,8,-1}, {-1,-1,-1}}; //Bordes
-double kernel[3][3] = {{0,-1,0}, {-1,5,-1}, {0,-1,0}}; //Sharpening
+ double kernel[3][3] = {{-1,-1,-1}, {-1,8,-1}, {-1,-1,-1}}; //Bordes
+//double kernel[3][3] = {{0,-1,0}, {-1,5,-1}, {0,-1,0}}; //Sharpening
 // double kernel[3][3] = {{(double)1/16,(double)1/8,(double)1/16}, {(double)1/8,(double)1/4,(double)1/8}, {(double)1/16,(double)1/8,(double)1/16}}; //Gaussian blur
 
 
@@ -38,12 +38,9 @@ void read_frames(struct Frame* frame, int size) {
 	}
 }
 
-void process_convolution(struct Frame* frame, struct Frame* out, int intensity){
+void process_convolution(struct Frame* frame, uint8_t* data_out, int intensity){
     int rows = frame->height;
     int cols = frame->width;
-    (*out).height = rows;
-    (*out).width = cols;
-    (*out).data = malloc(rows*cols*3*sizeof(uint8_t));
 	for (int i = 0; i < intensity; i++) {
 //         printf("Intensity %d\n",i);
 		//find center position of kernel
@@ -53,9 +50,9 @@ void process_convolution(struct Frame* frame, struct Frame* out, int intensity){
 		{
 			for (int j = 0; j < cols; ++j)				// columns
 			{
-                (*out).data[(i*cols+j)*3] = 0;
-                (*out).data[(i*cols+j)*3+1] = 0;
-                (*out).data[(i*cols+j)*3+2] = 0;
+                data_out[(i*cols+j)*3] = 0;
+                data_out[(i*cols+j)*3+1] = 0;
+				data_out[(i*cols+j)*3+2] = 0;
 				for (int m = 0; m < kRows; ++m)			// kernel rows
 				{
 
@@ -69,26 +66,26 @@ void process_convolution(struct Frame* frame, struct Frame* out, int intensity){
 						//ignore input samples which are out of bound
 						if(ii >= 0 && ii < rows && jj >= 0 && jj < cols){
 
-							(*out).data[(i*cols+j)*3] += (*frame).data[(ii*cols + jj)*3] * kernel[m][n];
-							(*out).data[((i*cols+j)*3)+1] += (*frame).data[((ii*cols + jj)*3)+1] * kernel[m][n];
-							(*out).data[((i*cols+j)*3)+2] += (*frame).data[((ii*cols + jj)*3)+2] * kernel[m][n];
+							data_out[(i*cols+j)*3] += (*frame).data[(ii*cols + jj)*3] * kernel[m][n];
+							data_out[((i*cols+j)*3)+1] += (*frame).data[((ii*cols + jj)*3)+1] * kernel[m][n];
+							data_out[((i*cols+j)*3)+2] += (*frame).data[((ii*cols + jj)*3)+2] * kernel[m][n];
 						}
 					}
 				}
 			}
 		}
-		*frame = *out;
+		//*frame = *out;
 	}
 }
 
-void applyFilter(int size, struct Frame* frames, struct Frame* out) {
+void applyFilter(int size, struct Frame* frames, uint8_t* data_out) {
 	printf("Aplicando filtro....\n");
 	char filename[300];
 	clock_t start, end;
 	start = clock();
 	int intensidad = 1;
 	for (int i = 0; i < size-1; ++i) {
-		process_convolution(&frames[i], &out[i], intensidad);
+		process_convolution(&frames[i], &data_out[i*frames[i].width * frames[i].height * 3], intensidad);
 	}
 	end = clock();
 	printf("Tiempo total kernel secuencial: %f\n",((double) (end-start)/CLOCKS_PER_SEC));
@@ -98,7 +95,7 @@ void applyFilter(int size, struct Frame* frames, struct Frame* out) {
 		sprintf(filename, "thumb%d.jpg",i+1);
         char ruta [300];
         sprintf(ruta, "pics2/%s",filename);
-        stbi_write_jpg(ruta, out[i].width, out[i].height, 3, out[i].data, out[i].width*3);
+        stbi_write_jpg(ruta, frames[i].width, frames[i].height, 3, &data_out[i*frames[i].width * frames[i].height * 3], frames[i].width*3);
     }
 }
 
@@ -110,12 +107,12 @@ int main(int argc, char* argv[])
 	}
 	//Sacar los fotogramas del video usando FFMPEG
 	char *filename = argv[1];
-//  	system("mkdir pics");
+  	system("mkdir pics");
 	system("mkdir pics2");
 	char *auxCommand = "pics/thumb%d.jpg -hide_banner";
 	char comando[300];
  	sprintf(comando, "ffmpeg -i %s.mp4 %s",filename,auxCommand);
-// 	system(comando);
+ 	system(comando);
 	sprintf(comando,"ffmpeg -i %s.mp4 -vn -acodec copy audio.aac",filename);
 	system(comando);
 
@@ -135,8 +132,13 @@ int main(int argc, char* argv[])
 	struct Frame fotogramas[frames-2];
 	read_frames(&fotogramas[0],frames-1);
 	int intensidad = 6;
-    struct Frame output[frames-2];
-	applyFilter(frames-1, &fotogramas[0], &output[0]);
+    uint8_t* data_out = malloc(fotogramas[0].width * fotogramas[0].height *3*sizeof(uint8_t) * frames-2);
+    if(data_out == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return -1;
+    }
+	applyFilter(frames-2, &fotogramas[0], data_out);
 	auxCommand = "ffmpeg -framerate 25 -i pics2/thumb%d.jpg";
 	sprintf(comando, "%s -pattern_type glob -c:v libx264 -pix_fmt yuv420p %s_out_provisional.mp4",auxCommand, filename);
 	system(comando);
